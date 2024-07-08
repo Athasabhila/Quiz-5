855d6dfaed5ca654161e5d0e79eb7ecd8453b384
Aulia Thalita S <168273405+Athasabhila@users.noreply.github.com>
2024-07-08 09:47:41 +0700

    Add files via upload

---
+++ PraUAS2.py
@@ -0,0 +1,10 @@
+capitals = {}
+
+capitals['Murica'] = 'Warshington'
+capitals['Germany'] = 'Bonn'
+capitals['France'] = 'Paris'
+capitals['Engalnd'] = 'London'
+
+capitals['Germany'] = 'Berlin'
+
+print("Capital Germany:", capitals['Germany'])
---
+++ PraUAS4.py
@@ -0,0 +1,7 @@
+letters = ["a", "b", "o", "c", "p"]
+
+
+print(letters[1])
+print(letters[len(letters) - 2])
+print(letters + ["x"])
+print(letters)
